2f208c6eca708b5ce539c320f5a5e138f42908af
Partha <parthaf22@gmail.com>
2018-05-10 22:15:49 +0530

    1st test

--- RestApiAutomation/UnitTest1.cs
+++ RestApiAutomation/UnitTest1.cs
@@ -24,7 +24,25 @@ namespace RestApiAutomation
 			var res = client.Execute(req);
 
 			var content = res.Content;
-			List< Dictionary<string,string>> holder=JsonConvert.DeserializeObject<List<Dictionary < string,string>>> (content);
+			  Dictionary<string,string> holder=JsonConvert.DeserializeObject<Dictionary < string,string>> (content);
+
+		}
+		
+			[TestMethod]
+		public void TestMethod2()
+		{
+			var client = new RestClient("http://api.football-data.org/");
+
+			var req = new RestRequest(Method.GET);
+
+			req.Resource = "v1/competitions/?season=2015";
+			//req.AddParameter("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI1MDAwMDAwMDAwMDAxMzI5Iiwicm9sZSI6IjMiLCJGaXJzdE5hbWUiOiJ2Y2RhdXRvIiwiTGFzdE5hbWUiOiJCREQiLCJVc2VySUQiOiI1MDAwMDAwMDAwMDAxMzI5IiwiQ291cnNlSUQiOiIxMDA1MzE4MiIsIlRpbWVab25lSUQiOiI0OSIsIkFjbElEIjoiODAwMDAwMDAwMDAwNTM0MyIsIm5iZiI6MTUyNDA0MDgwOCwiZXhwIjoxNTI0MDUxOTA4LCJpYXQiOjE1MjQwNDA4MDgsImlzcyI6IlBlZ2FzdXMiLCJhdWQiOiJQZWdhc3VzTm90aWZpY2F0aW9ucyJ9.TmKCPrb2VJuNkRDb3tDmw2EfzY909wiY_txB_XdbCr8");
+			//req.AddHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI1MDAwMDAwMDAwMDAxMzI5Iiwicm9sZSI6IjMiLCJGaXJzdE5hbWUiOiJ2Y2RhdXRvIiwiTGFzdE5hbWUiOiJCREQiLCJVc2VySUQiOiI1MDAwMDAwMDAwMDAxMzI5IiwiQ291cnNlSUQiOiIxMDA1MzE4MiIsIlRpbWVab25lSUQiOiI0OSIsIkFjbElEIjoiODAwMDAwMDAwMDAwNTM0MyIsIm5iZiI6MTUyNDA0MDgwOCwiZXhwIjoxNTI0MDUxOTA4LCJpYXQiOjE1MjQwNDA4MDgsImlzcyI6IlBlZ2FzdXMiLCJhdWQiOiJQZWdhc3VzTm90aWZpY2F0aW9ucyJ9.TmKCPrb2VJuNkRDb3tDmw2EfzY909wiY_txB_XdbCr8");
+
+			var res = client.Execute(req);
+
+			var content = res.Content;
+			List<Dictionary<string, string>> holder = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(content);
 
 		}
 	}
